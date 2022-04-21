0d0e49bd4fd99b3bfa77c0e73fb63aab122a030c
Vedant Poddar <vedant.poddar@adypu.edu.in>
2022-04-21 21:08:36 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,89 @@
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from PIL import Image
+import streamlit as st
+st.set_page_config(layout="wide")
+col1,col2,col3 = st.columns(3)
+image = Image.open("logo.png")
+with col1:
+    st.write("")
+
+with col2:
+    st.image(image, use_column_width=True)
+
+with col3:
+    st.write("")
+
+
+st.markdown("<h1 style='text-align: center;'>Detecting Diabetes using Machine Learning and Python</h1>", unsafe_allow_html=True) 
+st.sidebar.markdown("<h1 style='text-align: center;'>User Input</h1>", unsafe_allow_html=True)
+
+col1,col2 = st.columns(2)
+df = pd.read_csv("Datasets.csv") #   Data processing 
+with col1:
+    st.subheader("Dataset: ")
+    st.dataframe(df)
+with col2:
+    st.subheader("Data Description:")
+    st.write(df.describe())
+
+st.subheader("Bar Chart: ")
+chart = st.bar_chart(df)
+
+X = df.iloc[:, 0:8].values
+Y = df.iloc[:, -1].values
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
+
+
+def get_user_input():
+    pregnancies = st.sidebar.slider("Pregnancies", 0, 17, 3)
+    glucose = st.sidebar.slider("Glucose", 0, 199, 117)
+    blood_pressure = st.sidebar.slider("Blood Pressure", 0, 122, 72)
+    skin_thickness = st.sidebar.slider("Skin Thickness", 0, 99, 23)
+    insulin = st.sidebar.slider("Insulin", 0.0, 846.0, 30.0)
+    BMI = st.sidebar.slider("BMI", 0.0, 67.1, 32.0)
+    diabetes_pedigree_function = st.sidebar.slider("Diabetes Pedigree Function", 0.78, 2.42, 0.375)
+    age = st.sidebar.slider("Age", 21, 82, 45)
+
+    user_data = {"pregnancies": pregnancies,
+                 "glucose": glucose,
+                 "blood_pressure": blood_pressure,
+                 "skin_thickness": skin_thickness,
+                 "insulin": insulin,
+                 "BMI": BMI,
+                 "diabetes_pedigree_function": diabetes_pedigree_function,
+                 "age": age
+                 }
+    features = pd.DataFrame(user_data, index=[0])
+    return features
+
+
+user_input = get_user_input()
+
+st.subheader("User Input:")
+st.write(user_input)
+
+RandomForestClassifier = RandomForestClassifier()
+KNN = KNeighborsClassifier(13)
+KNN.fit(X_train, Y_train)
+RandomForestClassifier.fit(X_train, Y_train)
+
+
+st.subheader("Model Test Accuracy Score: ")
+st.write("Random Forest: " +str(accuracy_score(Y_test, RandomForestClassifier.predict(X_test)) * 100) + "%")
+st.write("K Neighbors: " +str(accuracy_score(Y_test, KNN.predict(X_test)) * 100) + "%")
+rfc_pred = RandomForestClassifier.predict(user_input)
+knn_pred = KNN.predict(user_input)
+st.subheader("Classification: ")
+col1,col2=st.columns(2)
+with col1:
+    st.write("Random Forest:")
+    st.write(rfc_pred)
+
+with col2:
+    st.write("K Neighbors:")
+    st.write(knn_pred)
